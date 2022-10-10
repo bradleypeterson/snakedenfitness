b247dd9813444fb57c6a25fbb3fe05d635e165c1
Whitney Smith <whitnaaays@gmail.com>
2022-10-10 15:53:42 -0600

    Workout form and template created

--- snakedenfitness/fitness/views.py
+++ snakedenfitness/fitness/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 
+from .forms import WorkoutForm
+
 # Create your views here.
 def fitness_home(request):
     return render(request, 'fitness/fitness_home.html', {})
@@ -8,6 +10,24 @@ def fitness_home(request):
 
 def trainer_home(request):
     return render(request, 'fitness/trainer_home.html', {})
+
+def workout_form(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = WorkoutForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('fitness/workout_form/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = WorkoutForm()
+
+    return render(request, 'fitness/workout_form.html', {'form': form})
 
 
 def update_profile(request, user_id):
