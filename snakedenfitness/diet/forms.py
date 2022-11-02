5fd87f2eb5978704982fa794f4a29b92ee637f6d
Whitney Smith <whitnaaays@gmail.com>
2022-11-02 12:25:14 -0600

    Updated styling of diet and workout forms

--- snakedenfitness/diet/forms.py
+++ snakedenfitness/diet/forms.py
@@ -9,10 +9,9 @@ class MealForm(forms.Form):
         ('snack', 'Snack'),
     ]
 
-    meal_name = forms.CharField(max_length=100)
-    meal_type = forms.CharField(label='Select meal type', widget=forms.Select(choices=MEAL_TYPES))
-    calories = forms.IntegerField()
-    carbs = forms.IntegerField()
-    sugars = forms.IntegerField()
-    protein = forms.IntegerField()
-
+    meal_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control huge', 'placeholder': 'Meal Name'}))
+    meal_type = forms.CharField(label='Select meal type', widget=forms.Select(choices=MEAL_TYPES, attrs={'class': 'form-select huge'}))
+    calories = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control huge', 'placeholder': 'Calories'}))
+    carbs = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control huge', 'placeholder': 'Carbs'}))
+    sugars = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control huge', 'placeholder': 'Sugars'}))
+    protein = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control huge', 'placeholder': 'Protein'}))
